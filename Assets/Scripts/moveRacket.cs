fab54311cabb2fe05f8e50cee3f2cf82f9988ca3
Shikhar Gaur <shikhar.gaur.13@gmail.com>
2015-05-30 19:04:36 -0400

    working Scene

---
+++ Assets/Scripts/moveRacket.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveRacket : MonoBehaviour {
+
+	public float speed = 30;
+	public string axis = "Vertical";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called at the same rate physics engine works 
+	void FixedUpdate () {
+		float dir = Input.GetAxisRaw (axis);
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, dir) * speed;
+	
+	}
+}
---
+++ Assets/Scripts/paddleRight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class paddleRight : MonoBehaviour {
+
+	public float speed = 30;
+	public string axis = "Vertical";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float direction = Input.GetAxisRaw ("Vertical");
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, direction) * speed;
+	}
+}
