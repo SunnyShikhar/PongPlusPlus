41cae8fc37cdb0016af7e4fea77829b5dcd6e322
Janujan Selvaratnam <janujan.selva@gmail.com>
2015-05-31 16:31:47 -0400

    Fixed music repeat issue and added credits scene

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	public GameObject camera;
+	public int speed = 1;
+	public int waitTime = 15;
+	public int level = -1;
+	float startTime = 0;
+	// Update is called once per frame
+	void Update () {
+
+		camera.transform.Translate (Vector3.down * Time.deltaTime * speed);
+		Debug.Log (Time.time - startTime);
+		if (Time.time - startTime >= waitTime) {
+			Application.LoadLevel(level);
+		}
+	}
+
+	void Start(){
+		startTime = Time.time;
+	}
+}
--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -3,8 +3,20 @@ using System.Collections;
 
 public class DontDestroy : MonoBehaviour {
 
+	private static DontDestroy instance =  null;
+
+	public static DontDestroy Instance{
+		get{ return instance; }
+	}
 	void Awake(){
-		DontDestroyOnLoad (gameObject);
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} 
+		else{
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
 	}
 
 }
